5ce0be075e6a901963a9bac51b2afaa1e6237b6c
Dhawal Malot <dhawalmalot08@gmail.com>
2024-06-22 18:14:22 +0530

    Learnt about react forms and FormData

--- forms-project/src/components/Login.jsx
+++ forms-project/src/components/Login.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
+import { useRef } from "react";
+
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState();
+  const email = useRef();
+  const password = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+    console.log("Email is valid: " + emailIsValid)
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    console.log("Sending HTTP request!");
+    setEmailIsInvalid(false);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
